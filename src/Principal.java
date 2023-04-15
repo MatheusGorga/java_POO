c400fa222e59c7fa7b2225dcb05829e34f611ed1
MatheusGorga <matheusgobar@gmail.com>
2023-04-15 17:09:07 -0300

    Quinto commit: reaproveitando caracteristicas e comportamentos

--- src/Principal.java
+++ src/Principal.java
@@ -1,19 +1,33 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
 
+        Filme meuFilme = new Filme();
         meuFilme.setNome("John Wick");
         meuFilme.setAnoDeLancamento(2017);
         meuFilme.setDuracaoEmMinutos(120);
 
-        meuFilme.exibeFichaTecnica();
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2017);
+        outroFilme.setDuracaoEmMinutos(220);
 
-        meuFilme.avalia(8);
-        meuFilme.avalia(5);
-        meuFilme.avalia(10);
-        System.out.println(meuFilme.getAvaliacao() + " " + meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.pegaMedia());
+        Serie lost = new Serie();
+        lost.setNome("lost");
+        lost.setTemporadas(10);
+        lost.setEpisodioPorTemporada(20);
+        lost.setMinutosPorEpisodio(50);
+
+
+
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println("Tempo total do filme = " + calculadora.getTempoTotal());
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,77 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private  int anoDeLancamento;
-    private  boolean incluidoNoPlano;
-    private  double somaAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends  Titulo{
+  private String diretor;
 
-    public String getNome() {
-        return nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public double getAvaliacao() {
-        return somaAvaliacoes;
-    }
-
-    public void setAvaliacao(double avaliacao) {
-        this.somaAvaliacoes = avaliacao;
-    }
-
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
-        this.totalDeAvaliacoes = totalDeAvaliacoes;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.println("#########################################");
-
-        System.out.println("Nome do filme = " + getNome());
-        System.out.println("Ano de lançamento = " + getAnoDeLancamento());
-        System.out.println("Duração = " + getDuracaoEmMinutos());
-
-        System.out.println("#########################################");
-    }
-
-    public void avalia(double nota){
-        somaAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return getAvaliacao() / getTotalDeAvaliacoes();
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
