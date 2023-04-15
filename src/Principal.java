0b88e11d7e58b6770fd89e9bcea1685e56bc1336
MatheusGorga <matheusgobar@gmail.com>
2023-04-15 15:20:32 -0300

    segundo commit: orientação a objetos

---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+    Filme meuFilme = new Filme();
+
+    meuFilme.setNome("John Wick");
+        System.out.println(meuFilme.getNome());
+
+    }
+}
