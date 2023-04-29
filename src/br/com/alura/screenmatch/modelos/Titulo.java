9ebba3d15093d40681b88658f39c420ede1e70cc
MatheusGorga <matheusgobar@gmail.com>
2023-04-29 20:18:59 -0300

    final do projeto

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,17 +1,30 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
     private String nome;
     private  int anoDeLancamento;
     private  boolean incluidoNoPlano;
     private  double somaAvaliacoes;
     private int totalDeAvaliacoes;
+    //@SerializedName("Runtime")
     private int duracaoEmMinutos;
 
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
     }
+
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        if(meuTituloOmdb.year().length() > 4) {
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano");
+        }
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
+    }
+
 
     public String getNome() {
         return nome;
@@ -83,5 +96,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return   "nome= '" + nome + '\'' +
+                ", anoDeLancamento= " + anoDeLancamento + '\'' +
+                ", duração= " + duracaoEmMinutos;
     }
 }
